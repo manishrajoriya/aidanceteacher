9c8e2e98a35e56d2e43ce6e9e55d0bb0ef67f7f6
manish <gkrajoriya3@gmail.com>
2025-01-18 11:10:47 +0530

    react native

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Index = () => {
+    return (
+        <View>
+            <Text>Index</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Index;
